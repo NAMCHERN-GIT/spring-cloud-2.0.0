9c086e34d0fca1ecdf3cacedf9fd8c0b8c01dbb5
chen.nan <namchern@aliyun.com>
2019-12-01 18:22:05 +0800

    提交微服务认证与授权修改信息

--- user-server/src/main/java/com/chennan/cloud/user/cfg/ResourceServerConfig.java
+++ user-server/src/main/java/com/chennan/cloud/user/cfg/ResourceServerConfig.java
@@ -13,7 +13,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         //http.formLogin().loginPage("/login").permitAll().and().logout().logoutUrl("/logout").permitAll();
-            http.csrf().disable()   
+            http.csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
                 .and()
