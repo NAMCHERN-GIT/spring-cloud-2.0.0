7bc0b5266b358337f36f9e1a37c0cdce2f07cd44
chen.nan <namchern@aliyun.com>
2019-12-01 18:21:28 +0800

    提交微服务认证与授权修改信息

--- user-server/src/main/java/com/chennan/cloud/user/UserServer.java
+++ user-server/src/main/java/com/chennan/cloud/user/UserServer.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableDistributedTransaction
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UserServer {
     public static void main(String[] args) {
         SpringApplication.run(UserServer.class, args);
--- user-server/src/main/java/com/chennan/cloud/user/cfg/ResourceServerConfig.java
+++ user-server/src/main/java/com/chennan/cloud/user/cfg/ResourceServerConfig.java
@@ -12,7 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable()
+        //http.formLogin().loginPage("/login").permitAll().and().logout().logoutUrl("/logout").permitAll();
+            http.csrf().disable()   
                 .exceptionHandling()
                 .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
                 .and()
