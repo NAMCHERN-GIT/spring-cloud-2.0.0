1f268870ed1eabed9cd4628769a8f3312fe401b1
chen.nan <namchern@aliyun.com>
2019-12-25 12:48:00 +0800

    提交分布式任务调度框架xxl-job-admin

---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/exception/XxlJobException.java
@@ -0,0 +1,19 @@
+package com.xxl.job.admin.core.exception;
+
+/**
+ * @author xuxueli 2019-05-04 23:19:29
+ */
+public class XxlJobException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public XxlJobException() {
+    }
+    public XxlJobException(String message) {
+        super(message);
+    }
+
+}
