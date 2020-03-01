0730a41477119518f6c8a40b1af96a7bbd03f524
chen.nan <namchern@aliyun.com>
2020-03-01 21:02:07 +0800

    代码注释完善

--- elasticsearch-api/src/main/java/com/chennan/cloud/es/base/annotation/Document.java
+++ elasticsearch-api/src/main/java/com/chennan/cloud/es/base/annotation/Document.java
@@ -1,7 +1,14 @@
 package com.chennan.cloud.es.base.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.Documented;
 
+/**
+ * elasticsearch doc 文档实体标注类
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
--- elasticsearch-api/src/test/java/com/chennan/cloud/junit/bo/Book.java
+++ elasticsearch-api/src/test/java/com/chennan/cloud/junit/bo/Book.java
@@ -17,25 +17,18 @@ import java.util.Date;
 @Document(index = "book")
 public class Book implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private String id;              // 主键
-
     private String bookName;        // 书名
-
     private String author;          // 作者
-
     private String serialNumber;    // 书籍序列号
-
     private String publisher;       // 出版社
-
     @JSONField(format = "yyyy-MM-dd")
     private Date publicationDate;   // 出版日期
-
     private String editionNumber;   // 版次
-
     private Integer wordCount;      // 字数
-
     private BigDecimal price;       // 定价
-
-    private Instant createTime;        // 创建时间
+    private Instant createTime;     // 创建时间
 
 }
